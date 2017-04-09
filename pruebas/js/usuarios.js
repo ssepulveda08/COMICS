f90b2d83d0fb308ffb41a90fc279a7ada636e4d8
ruby <santiago.sc08@gmail.com>
2017-04-08 21:08:59 -0500

    no message

---
+++ pruebas/js/usuarios.js
@@ -0,0 +1,31 @@
+
+	  //user.nombre = nombre;
+	  //user.contrasena = contrasena;
+	  //user.correo = correo;
+	  //user.rol = rol;
+	  //console.log(user)
+
+ var users = [
+				{ nombre:"andres", contrasena:"santago",correo:"212ad",rol:"1"},
+				{ nombre:"asdasd",contrasena:"poopp",correo:"dasdasd", rol:"2"},
+			]
+
+
+function newUser(nombre, contrasena, correo, rol) {
+
+		var c = users.length;		
+		this.nombre = nombre;
+	  	this.contrasena = contrasena;
+		this.correo = correo;
+		this.rol = rol;
+
+		users.push(this);
+					
+}
+
+
+function mostrarUser() {
+	for( var i = 0 ;i<=users.length;i++ ){
+		console.log(users[i])
+	}
+}
